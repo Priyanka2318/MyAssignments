1c47244a3695a95a91006e8cbd7c30af7e3a95d4
Priyanka2318 <134782536+Priyanka2318@users.noreply.github.com>
2023-05-27 15:55:56 +0400

    Add files via upload
    Day1

---
+++ day1/Vehicle.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+		
+		int vehicleNo = 2318;
+		long kilometers = 12500000l;
+		String type = "Diesel";
+		char logo = 'S';
+		
+		System.out.println("Vehicle Number:" + vehicleNo);
+		System.out.println("Kilometers:" + kilometers);
+		System.out.println("Vehicle Type:" + type);
+		System.out.println("Logo:" + logo);
+
+	}
+
+}
