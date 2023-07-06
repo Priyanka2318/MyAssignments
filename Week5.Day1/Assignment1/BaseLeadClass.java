258ed1dbe7d7d67c5a1c882fe623c755e1c47c07
Priyanka2318 <134782536+Priyanka2318@users.noreply.github.com>
2023-07-06 17:11:55 +0400

    Add files via upload

---
+++ Week5.Day1/Assignment1/BaseLeadClass.java
@@ -0,0 +1,25 @@
+package week5.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseLeadClass {
+	public ChromeDriver driver;
+	@BeforeMethod
+	public void preCondition() {
+		driver= new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+	}
+}
